09911df68237b8cacb394377e90f9b5040ade6c1
Leon Winter <lwi@ring0.de>
2012-12-03 16:19:53 +0900

    LocalShortestPathRoutingWithTrafficLights: working (based on LocalShortestPathRouting)

--- simulator/src/LocalShortestPathRoutingWithTrafficLights.java
+++ simulator/src/LocalShortestPathRoutingWithTrafficLights.java
@@ -1,57 +1,40 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.PriorityQueue;
 
 public class LocalShortestPathRoutingWithTrafficLights implements RoutingAlgorithm {
-	HashMap<Vehicle, ArrayList<PathEdge>> fixedRoutes = new HashMap<Vehicle, ArrayList<PathEdge>>();
 	
 	@Override
 	public Edge nextEdge(Vehicle vehicle, int tick) {
-		Edge current = vehicle.getPosition();
-		for (PathEdge e : fixedRoutes.get(vehicle)) {
-			if (e.getPrev().getEdge() == current) {
-				current = e.getEdge();
+		Edge edge = vehicle.getPosition();
+		ArrayList<Edge> visited = new ArrayList<Edge>();
+		PriorityQueue<PathEdge> distances = new PriorityQueue<PathEdge>();
+		PathEdge spawn = new PathEdge(null, 0, edge);
+		distances.add(spawn);
+		PathEdge target = null;
+		/* calculate path distances */
+		while (!distances.isEmpty()) {
+			PathEdge current = distances.poll();
+			if (current.getEdge().getOutgoingNode() == vehicle.getTarget()) {
+				target = current;
 				break;
 			}
+			for (Edge e : current.getEdge().getOutgoingNode().getOutgoingEdges()) {
+				if (visited.contains(e)) continue;
+				PathEdge d = new PathEdge(current, current.getEdge().getDistance() + e.getDistance() +
+						(e.getTrafficLight() == null ? 0 :e.getTrafficLight()
+							.remainingWaitingTime(tick + current.getEdge().getDistance() + e.getDistance())), e);
+				distances.offer(d);
+				visited.add(e);
+			}
 		}
-		return current;
+		/* build path to target node */
+		PathEdge ptr;
+		for (ptr = target; ptr.getPrev() != spawn; ptr = ptr.getPrev());
+		return ptr.getEdge();
 	}
 
 	@Override
 	public void init(Vehicle[] vehicles) {
-		for (Vehicle vehicle : vehicles) {
-			ArrayList<PathEdge> route = new ArrayList<PathEdge>();
-			
-			Edge edge = vehicle.getPosition();
-			ArrayList<Edge> visited = new ArrayList<Edge>();
-			PriorityQueue<PathEdge> distances = new PriorityQueue<PathEdge>();
-			PathEdge spawn = new PathEdge(null, 0, edge);
-			distances.add(spawn);
-			PathEdge target = null;
-			/* calculate path distances */
-			while (!distances.isEmpty()) {
-				PathEdge current = distances.poll();
-				if (current.getEdge().getOutgoingNode() == vehicle.getTarget()) {
-					target = current;
-					break;
-				}
-				for (Edge e : current.getEdge().getOutgoingNode().getOutgoingEdges()) {
-					if (visited.contains(e)) continue;
-					int dist = current.getEdge().getDistance() + e.getDistance();
-					if (e.getTrafficLight() != null)
-						dist += e.getTrafficLight().remainingWaitingTime(dist);
-					PathEdge d = new PathEdge(current, dist, e);
-					distances.offer(d);
-					visited.add(e);
-				}
-			}
-			/* build path to target node */
-			PathEdge ptr;
-			for (ptr = target; ptr.getPrev() != spawn; ptr = ptr.getPrev()) {
-				route.add(0, ptr);
-			}
-			fixedRoutes.put(vehicle, route);
-		}
 	}
 
 }
