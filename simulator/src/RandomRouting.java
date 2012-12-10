a7db3267be0cafb7671662704e46bcdbb47fe19d
Leon Winter <lwi@ring0.de>
2012-12-10 16:40:29 +0900

    RandomRouting: now really random

--- simulator/src/RandomRouting.java
+++ simulator/src/RandomRouting.java
@@ -1,8 +1,11 @@
+import java.util.Random;
+
 
 public class RandomRouting implements RoutingAlgorithm {
 	@Override
 	public Edge nextEdge(Vehicle vehicle, int tick) {
-		return vehicle.getPosition().getOutgoingNode().getOutgoingEdges()[0];
+		Edge[] edges = vehicle.getPosition().getOutgoingNode().getOutgoingEdges();
+		return edges[new Random().nextInt(edges.length)];
 	}
 
 	@Override
