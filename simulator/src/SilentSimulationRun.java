6be5f5a3a1afe64b56c156ec5f824498e9ef3e1a
Leon Winter <lwi@ring0.de>
2012-12-10 16:42:51 +0900

    SiltentSimulationRun: output car statistics

--- simulator/src/SilentSimulationRun.java
+++ simulator/src/SilentSimulationRun.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
 public class SilentSimulationRun {
@@ -36,7 +39,33 @@ public class SilentSimulationRun {
 			int i = 0;
 			for (i = 0; !sim.isFinished() && i < upperlimit; i++)
 				sim.progress();
-			System.out.printf("algorithm: %s took %d ticks to finish\n", r.getClass().toString(), i);
+
+			System.out.printf("[%s]\n", r.getClass().getName());
+			if (i == upperlimit)
+				System.out.println(" never finished");
+			else
+				System.out.printf(" time to finish: %d\n", i);
+			/* car stats */
+			ArrayList<Integer> carStats = sim.getVehicleFinishTimes();
+			Collections.sort(carStats);
+			if(carStats.size() > 0)
+			System.out.printf(" car stats:\n  min time: %d\n  max time: %d\n  avg time: %f\n  median: %f\n",
+				carStats.get(0), carStats.get(carStats.size() - 1), calculateAverage(carStats), calculateMedian(carStats));
 		}
+	}
+
+	public static double calculateAverage (List<Integer> values) {
+		int sum = 0;
+		for (int v : values)
+			sum += v;
+		return (double)sum / (double)values.size();
+	}
+
+	public static double calculateMedian (ArrayList<Integer> values)
+	{
+		if (values.size() % 2 == 1)
+			return values.get((values.size()+1)/2-1);
+		else
+			return (values.get(values.size()/2-1) + values.get(values.size()/2)) / 2.0;
 	}
 }
