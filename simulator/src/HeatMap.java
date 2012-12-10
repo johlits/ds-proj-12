293b00dd32b4681b413cb49685da3678bd134b74
Leon Winter <lwi@ring0.de>
2012-12-11 00:01:20 +0900

    HeatMapRouting: route based on heatmap data

--- simulator/src/HeatMap.java
+++ simulator/src/HeatMap.java
@@ -1,18 +1,22 @@
 import java.util.HashMap;
 import java.util.Map.Entry;
 
-public class HeatMap {
-	private HashMap<Edge,Integer> map = new HashMap<Edge,Integer>();
+public class HeatMap<T> {
+	private HashMap<T,Integer> map = new HashMap<T,Integer>();
 	
-	public void increaseHeat (Edge e) {
+	public void increaseHeat (T e) {
 		if (!map.containsKey(e))
 			map.put(e, 0);
 		map.put(e, map.get(e) + 1);
 	}
 	
+	public int getHeat (T e) {
+		return map.containsKey(e) ? map.get(e) : 0;
+	}
+
 	public String toString () {
 		String s = "";
-		for (Entry<Edge, Integer> e : map.entrySet()) {
+		for (Entry<T, Integer> e : map.entrySet()) {
 			s += " " + e.getKey().toString() + ": " + e.getValue().toString() + "\n";
 		}
 		return s;
