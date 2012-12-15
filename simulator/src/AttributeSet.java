c8f7096601f7cc00d8601546012aa6ce23dcddae
Leon Winter <lwi@ring0.de>
2012-12-15 20:07:37 +0900

    AttributeSet: +toString

--- simulator/src/AttributeSet.java
+++ simulator/src/AttributeSet.java
@@ -5,4 +5,13 @@ public class AttributeSet {
 	int trafficLightOffset = -1;
 	int trafficLightGreenCycle;
 	int trafficLightRedCycle;
+
+	public String toString () {
+		String s = Integer.toString(distance) + " " + Integer.toString(capacity);
+		if (trafficLightOffset != -1)
+			s +=	" " + Integer.toString(trafficLightOffset)
+				+	" " + Integer.toString(trafficLightGreenCycle)
+				+	" " + Integer.toString(trafficLightRedCycle);
+		return s;
+	}
 }
